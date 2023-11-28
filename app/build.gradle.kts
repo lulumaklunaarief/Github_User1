7f073ff8a456d2e73ff9b602640e632e20371d3f
lulumaklunaarief <lmakluna1@gmail.com>
2023-11-28 22:09:58 +0700

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
 
 }
 
@@ -62,5 +64,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.room:room-runtime:2.5.2")
+    ksp("androidx.room:room-compiler:2.6.0")
 
 }
---
+++ app/src/main/java/com/dicoding/githubuser/data/roomdatabase/Favorite.kt
@@ -0,0 +1,9 @@
+//package com.dicoding.githubuser.data.roomdatabase
+//
+//import androidx.room.Entity
+//
+//data class Favorite {
+//    @Entity
+//
+//
+//}
